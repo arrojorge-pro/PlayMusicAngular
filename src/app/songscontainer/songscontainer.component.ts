0d574ceaad547ee051eb897a469d3e0b47b82257
arrojorge <arrojorge@uoc.edu>
2021-10-31 14:24:53 +0100

    Filtra por numCancion pero bug al mostrar listado

--- src/app/songscontainer/songscontainer.component.ts
+++ src/app/songscontainer/songscontainer.component.ts
@@ -10,24 +10,50 @@ import { SongComponent } from '../song/song.component';
 export class SongscontainerComponent implements OnInit {
 
   songs!:Song[];
+  selectSong!:Song;
 
   constructor() { 
-    this.songs =[
-                    (new SongComponent("mi carro","manolo",1956,"disco","estilo","caratula",1,1)),
-                    (new SongComponent("mi carro2","manolo2",19562,"disco2","estilo2","caratula2",12,12))
-  
-  
-                ];
+   
 
   }
 
   ngOnInit(): void {
+    this.songs =[
+      (new SongComponent("mi carro","manolo",1956,"disco","estilo","caratula",1,1)),
+      (new SongComponent("mi carro2","manolo2",19562,"disco2","estilo2","caratula2",12,12))
+
+
+  ];
    
   }
 
-  public createSong(){
-    
+  public detalle(){
+    alert("hola");
   }
+
+
+
+  onItemSelector(value :any) {
+    console.log(value);
+    
+    }
+
+    public getSelectSong(value :any){
+      if(value){
+        this.songs.forEach(song => {
+          if(song.numCancion==value){
+            this.selectSong = song;
+          }
+        });
+      }
+    }
+
+  // public sendSong(num:Number){
+  //   let boton = document.getElementById('detail');
+  //   if (boton.value==num){
+
+  //   }
+  // }
 
   // public add (song:Song){
   //   this.songs.push(song);
