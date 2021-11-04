176b0a6feef9c21d195090856c922270395a3df9
arrojorge <arrojorge@uoc.edu>
2021-11-04 22:59:09 +0100

    Volcado de detalles en vista. Falta Caratula en src

--- src/app/caratula/caratula.component.ts
+++ src/app/caratula/caratula.component.ts
@@ -10,26 +10,15 @@ import { SongscontainerComponent } from '../songscontainer/songscontainer.compon
 export class CaratulaComponent implements OnInit {
 
  @Input() cancion!:Song;
- static canc:Song;
 
   constructor() {
     
+  
     // CaratulaComponent.cancion = SongscontainerComponent.getSelect();
 
    }
 
   ngOnInit(): void {
-  }
-
-  public setCancion(song:Song){
-    this.cancion = song;
-    if(CaratulaComponent.canc)
-      console.log("ha pasado por canc notnull");
-      this.cancion = CaratulaComponent.canc
-  }
-
-  public static setCancion2(song:Song){
-    CaratulaComponent.canc = song;
   }
 
   public getCancion(){
