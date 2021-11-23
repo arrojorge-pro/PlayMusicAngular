f1318849a1eea15a3e38a8adf97d200fc8dbb213
arrojorge <arrojorge@uoc.edu>
2021-11-23 16:52:18 +0100

    peleando bucles JSON

---
+++ src/app/dataSongs/DataSongs.ts
@@ -0,0 +1,48 @@
+
+
+const canciones = [
+{
+  "title":'Pokusone',
+  "artis":'Collocutor',
+  "anio":2019,
+  "disco":'Kaleidosope',
+  "estilo":'Jazz',
+  "caratula":"assets/music/1/kaleidoscope.jpg",
+  "numdisco":1,
+  "numCancion":1,
+  "url":"assets/music/1/pokusone.mp3"
+},{
+  "title":'All you need is love',
+  "artis":"Beattles",
+  "anio":1996,
+  "disco":"Smooth Jazz",
+  "estilo":"Jazz",
+  "caratula":"assets/music/2/beattles.jpg",
+  "numdisco":2,
+  "numCancion":2,
+  "url":"assets/music/2/allYouNeedIsLove.mp3"
+
+},
+{
+  "title":"Thriller",
+  "artis":"Michael Jackson",
+  "anio":1989,
+  "disco":"Smooth Jazz",
+  "estilo":"Jazz",
+  "caratula":"assets/music/3/jackson.jpg",
+  "numdisco":3,
+  "numCancion":3,
+  "url":"assets/music/3/thriller.mp3"
+}
+]
+export default canciones;
+
+
+
+
+
+
+// (new SongComponent("Pokusone","Collocutor",2019,"Kaleidoscpe","Jazz","assets/music/1/kaleidoscope.jpg",1,1,"assets/music/1/pokusone.mp3")),
+// (new SongComponent("All you need is love","Beattles",1996,"Smooth Jazz","Jazz","assets/music/2/beattles.jpg",2,2,"assets/music/2/allYouNeedIsLove.mp3")),
+// (new SongComponent("Thriller","Michael Jackson",1989,"Smooth Jazz","Jazz","assets/music/3/jackson.jpg",3,3,"assets/music/3/thriller.mp3")),
+// (new SongComponent("Bohemian Rapsody","Queen",1981,"The Songs Of Queen","Jazz","assets/music/4/queen.jpg",4,4,"assets/music/4/bohemian.mp3")),
--- src/app/songscontainer/songscontainer.component.ts
+++ src/app/songscontainer/songscontainer.component.ts
@@ -3,6 +3,7 @@ import { AppComponent } from '../app.component';
 import { CaratulaComponent } from '../caratula/caratula.component';
 import { Song } from '../Song';
 import { SongComponent } from '../song/song.component';
+import canciones from '../dataSongs/DataSongs';
 
 
 @Component({
@@ -18,13 +19,53 @@ export class SongscontainerComponent implements OnInit {
   search = '';
 
   constructor() { 
-    this.songs =[
-      (new SongComponent("Pokusone","Collocutor",2019,"Kaleidoscpe","Jazz","assets/music/1/kaleidoscope.jpg",1,1,"assets/music/1/pokusone.mp3")),
-      (new SongComponent("All you need is love","Beattles",1996,"Smooth Jazz","Jazz","assets/music/2/beattles.jpg",2,2,"assets/music/2/allYouNeedIsLove.mp3")),
-      (new SongComponent("Thriller","Michael Jackson",1989,"Smooth Jazz","Jazz","assets/music/3/jackson.jpg",3,3,"assets/music/3/thriller.mp3")),
-      (new SongComponent("Bohemian Rapsody","Queen",1981,"The Songs Of Queen","Jazz","assets/music/4/queen.jpg",4,4,"assets/music/4/bohemian.mp3")),
+    
+    console.log(canciones);
+    
+    for (var _i = 0; _i < canciones.length; _i++) {
+      console.log(_i);
+      console.log(canciones[_i].title);
+      console.log(canciones[_i].artis);
+      console.log(canciones[_i].anio);
+      console.log(canciones[_i].disco);
+      console.log(canciones[_i].estilo);
+      console.log(canciones[_i].caratula);
+      console.log(canciones[_i].numdisco);
+      console.log(canciones[_i].numCancion);
+      console.log(canciones[_i].url);
+      //this.songs[_i]= new SongComponent(canciones[_i].title,canciones[_i].artis,canciones[_i].anio,canciones[_i].disco,canciones[_i].estilo,canciones[_i].caratula,canciones[_i].numdisco,canciones[_i].numCancion,canciones[_i].url);
+    }
+    
+    this.songs= [
+      (new SongComponent(canciones[1].title,canciones[1].artis,canciones[1].anio,canciones[1].disco,canciones[1].estilo,canciones[1].caratula,canciones[1].numdisco,canciones[1].numCancion,canciones[1].url)),
+      (new SongComponent(canciones[2].title,canciones[2].artis,canciones[2].anio,canciones[2].disco,canciones[2].estilo,canciones[2].caratula,canciones[2].numdisco,canciones[2].numCancion,canciones[2].url)),
+      (new SongComponent(canciones[3].title,canciones[3].artis,canciones[3].anio,canciones[3].disco,canciones[3].estilo,canciones[3].caratula,canciones[3].numdisco,canciones[3].numCancion,canciones[3].url)),
+//      (new SongComponent(canciones[4].title,canciones[4].artis,canciones[4].anio,canciones[4].disco,canciones[4].estilo,canciones[4].caratula,canciones[4].numdisco,canciones[4].numCancion,canciones[4].url))
+      ];
+
+    canciones.forEach(cancion => {
+      //this.songs=[new SongComponent(cancion.title,cancion.artis,cancion.anio,cancion.disco,cancion.estilo,cancion.caratula,cancion.numdisco,cancion.numCancion,cancion.url)];
+    });
+
+    // let temp!:Song[];
+    // canciones.forEach(function (cancion){
+    //  temp.push (new SongComponent(cancion.title,cancion.artis,cancion.anio,cancion.disco,cancion.estilo,cancion.caratula,cancion.numdisco,cancion.numCancion,cancion.url));
+    // });
+
+    // this.songs = temp;
+  //   this.songs =[
+
       
-  ];
+
+      
+
+     
+  //     // (new SongComponent("Pokusone","Collocutor",2019,"Kaleidoscpe","Jazz","assets/music/1/kaleidoscope.jpg",1,1,"assets/music/1/pokusone.mp3")),
+  //     // (new SongComponent("All you need is love","Beattles",1996,"Smooth Jazz","Jazz","assets/music/2/beattles.jpg",2,2,"assets/music/2/allYouNeedIsLove.mp3")),
+  //     // (new SongComponent("Thriller","Michael Jackson",1989,"Smooth Jazz","Jazz","assets/music/3/jackson.jpg",3,3,"assets/music/3/thriller.mp3")),
+  //     // (new SongComponent("Bohemian Rapsody","Queen",1981,"The Songs Of Queen","Jazz","assets/music/4/queen.jpg",4,4,"assets/music/4/bohemian.mp3")),
+      
+  // ];
     
 
   }
