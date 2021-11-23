efba7c17bf8f79c98bc36d54bf1a6cec13886a6b
arrojorge <arrojorge@uoc.edu>
2021-11-23 17:31:10 +0100

    runs

--- src/app/songscontainer/songscontainer.component.ts
+++ src/app/songscontainer/songscontainer.component.ts
@@ -37,10 +37,10 @@ export class SongscontainerComponent implements OnInit {
     }
     
     this.songs= [
+      (new SongComponent(canciones[0].title,canciones[0].artis,canciones[0].anio,canciones[0].disco,canciones[0].estilo,canciones[0].caratula,canciones[0].numdisco,canciones[0].numCancion,canciones[0].url)),
       (new SongComponent(canciones[1].title,canciones[1].artis,canciones[1].anio,canciones[1].disco,canciones[1].estilo,canciones[1].caratula,canciones[1].numdisco,canciones[1].numCancion,canciones[1].url)),
       (new SongComponent(canciones[2].title,canciones[2].artis,canciones[2].anio,canciones[2].disco,canciones[2].estilo,canciones[2].caratula,canciones[2].numdisco,canciones[2].numCancion,canciones[2].url)),
-      (new SongComponent(canciones[3].title,canciones[3].artis,canciones[3].anio,canciones[3].disco,canciones[3].estilo,canciones[3].caratula,canciones[3].numdisco,canciones[3].numCancion,canciones[3].url)),
-//      (new SongComponent(canciones[4].title,canciones[4].artis,canciones[4].anio,canciones[4].disco,canciones[4].estilo,canciones[4].caratula,canciones[4].numdisco,canciones[4].numCancion,canciones[4].url))
+      (new SongComponent(canciones[3].title,canciones[3].artis,canciones[3].anio,canciones[3].disco,canciones[3].estilo,canciones[3].caratula,canciones[3].numdisco,canciones[3].numCancion,canciones[3].url))
       ];
 
     canciones.forEach(cancion => {
